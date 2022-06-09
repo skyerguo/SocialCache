16337630991baffb51239266eb69d29fe6483575
skyerguo97 <skyerguo97@gmail.com>
2022-06-09 15:10:12 +0800

    增加make_trace，合称为统一一个traces

--- code/main/main.py
+++ code/main/main.py
@@ -2,4 +2,18 @@ from code.build.build_main import Build_network
 from code.trace.make_trace import Make_trace
 from code.cache.redis_cache.redis_cache import Redis_cache
 
+trace_dir = 'naive'
+m = Make_trace(trace_dir)
+m.run()
+
 b = Build_network()
+b.run()
+
+## read_trace
+f_posts_in = open("data/traces/" + trace_dir + "/posts_timeline.txt", "r")
+f_views_in = open("data/traces/" + trace_dir + "/views_timeline.txt", "r")
+
+
+
+f_posts_in.close()
+f_views_in.close()
