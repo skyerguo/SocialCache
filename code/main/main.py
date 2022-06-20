20b937702b77dbc69d55a27b925e0bc3a937dbd9
skyerguo97 <skyerguo97@gmail.com>
2022-06-20 23:29:49 +0800

    LRU test error，to be fixed

--- code/main/main.py
+++ code/main/main.py
@@ -24,21 +24,25 @@ class Main:
         self.find_success_number = [0, 0, 0, 0]
         self.find_fail_number = [0, 0, 0, 0]
 
-    def reflush_cache(self):
+    def reflush_cache(self, use_LRU_cache=False):
         host_all = []
         for level_1_host_id in range(self.build_network.level_1_host_number):
-            self.build_network.level_1_host[level_1_host_id].redis_cache = Redis_cache(db=len(host_all), cache_size=1000)
+            self.build_network.level_1_host[level_1_host_id].redis_cache = Redis_cache(db=len(host_all), cache_size=1000, use_LRU_cache=use_LRU_cache)
             host_all.append(self.build_network.level_1_host[level_1_host_id])
         for level_2_host_id in range(self.build_network.level_2_host_number):
-            self.build_network.level_2_host[level_2_host_id].redis_cache = Redis_cache(db=len(host_all), cache_size=100)
+            self.build_network.level_2_host[level_2_host_id].redis_cache = Redis_cache(db=len(host_all), cache_size=100, use_LRU_cache=use_LRU_cache)
             host_all.append(self.build_network.level_2_host[level_2_host_id])
         for level_3_host_id in range(self.build_network.level_3_host_number):
-            self.build_network.level_3_host[level_3_host_id].redis_cache = Redis_cache(db=len(host_all), cache_size=10)
+            self.build_network.level_3_host[level_3_host_id].redis_cache = Redis_cache(db=len(host_all), cache_size=10, use_LRU_cache=use_LRU_cache)
             host_all.append(self.build_network.level_3_host[level_2_host_id])
 
-    def FIFO(self):
+    def Classical(self, specific_type):
         '''read_trace'''
-        self.reflush_cache()
+        if specific_type == "LRU":
+            self.reflush_cache(use_LRU_cache=True)
+        else:
+            self.reflush_cache()
+
         f_in = open("data/traces/" + self.trace_dir + "/all_timeline.txt", "r")
         for line in f_in:
             current_type = line.split('+')[-1].strip()
@@ -75,7 +79,7 @@ class Main:
         f_in.close()
 
         '''分析'''
-        print("缓存策略： *** FIFO ***")
+        print("缓存策略： *** %s ***"%(specific_type))
         print("三级CDN缓存命中率：", self.find_success_number[3] / (self.find_success_number[3] + self.find_fail_number[3]))
         print("二级CDN缓存命中率：", self.find_success_number[2] / (self.find_success_number[2] + self.find_fail_number[2]))
         print("一级CDN缓存命中率：", self.find_success_number[1] / (self.find_success_number[1] + self.find_fail_number[1]))
@@ -99,7 +103,6 @@ class Main:
             if current_type == "post":
                 post_id = int(line.split('+')[4])
                 user_id = int(line.split('+')[3])
-                # print(user_id, page_rank_metrics[str(user_id)], int(page_rank_metrics[str(user_id)] * 1000) + current_timestamp)
                 self.build_network.level_3_host[selected_level_3_id].redis_cache.insert(post_id, int(page_rank_metrics[str(user_id)] * 10000) + current_timestamp)
                 self.build_network.level_2_host[bind_level_2_id].redis_cache.insert(post_id, int(page_rank_metrics[str(user_id)] * 10000) + current_timestamp)
                 self.build_network.level_1_host[bind_level_1_id].redis_cache.insert(post_id, int(page_rank_metrics[str(user_id)] * 10000) + current_timestamp)
@@ -130,12 +133,14 @@ class Main:
 
     def run(self, caching_policy):
         if caching_policy == 'FIFO':
-            self.FIFO()
+            self.Classical()
+        elif caching_policy == "LRU":
+            self.Classical(specific_type="LRU")
         elif caching_policy == 'PageRank':
             self.PageRank()
-
 
 if __name__ == '__main__':
     main_program = Main()
     main_program.run('FIFO')
+    main_program.run('LRU')
     main_program.run('PageRank')
