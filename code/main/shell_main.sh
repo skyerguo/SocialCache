834de2ccef86fb0b66099b097c80a1bf142b4f58
skyerguo97 <skyerguo97@gmail.com>
2022-08-05 08:45:59 +0800

    update

--- code/main/shell_main.sh
+++ code/main/shell_main.sh
@@ -5,9 +5,9 @@ sleep 20
 cat code/main/basic_config.json | jq '.caching_policy="FIFO"' > code/main/config.json
 sudo python3 -m code.main.main
 
-sleep 20
-cat code/main/basic_config.json | jq '.caching_policy="RAND"' > code/main/config.json
-sudo python3 -m code.main.main
+# sleep 20
+# cat code/main/basic_config.json | jq '.caching_policy="RAND"' > code/main/config.json
+# sudo python3 -m code.main.main
 
 sleep 20
 cat code/main/basic_config.json | jq '.caching_policy="PageRank"' > code/main/config.json
