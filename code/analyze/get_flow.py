052c163654cec9533f83568a8651562e65af4817
skyerguo97 <skyerguo97@gmail.com>
2022-08-08 15:47:47 +0800

    修改代码分析，增加每层的流量统计

--- code/analyze/get_flow.py
+++ code/analyze/get_flow.py
@@ -1,12 +1,14 @@
 import argparse
 import os
 import json
+import math
 
 data_root = '/proj/socnet-PG0/data/'
 
 p = argparse.ArgumentParser(description='Analyze the result')
 p.add_argument('-t', '--timestamp', type=str, default="", dest="timestamp", action="store", help="the timestamp of data file")
 p.add_argument('-n', '--nearnumber', type=int, default=0, dest="nearNumber", action="store", help="the number of nearest timestamp")
+p.add_argument('-d', '--detailoutput', default=False, dest="detailOutput", action="store_true", help="give more detail outputs")
 args = p.parse_args()
 
 data_root = '/proj/socnet-PG0/data/'
@@ -18,9 +20,16 @@ else:
     flow_path = data_root + files[args.nearNumber] + '/flow/'
     
 total_flow = 0
+flow_each_level = [0 for _ in range(3)]
 for ip_address in os.listdir(flow_path):
     curr_path = flow_path + ip_address + '/'
+    curr_level = math.floor(int(ip_address.split('.')[-1]) / 2)
     for file_name in os.listdir(curr_path):
         for line in open(curr_path + file_name, 'r'):
             total_flow += int(line)
+            flow_each_level[curr_level] += int(line)
 print(total_flow)
+
+if args.detailOutput:
+    for i in range(3):
+        print("level %s flow %s"%(str(i), flow_each_level[i]))
--- code/analyze/get_media_size.py
+++ code/analyze/get_media_size.py
@@ -1,25 +1,40 @@
 import argparse
 import os
 import json
+import math
 
 p = argparse.ArgumentParser(description='Analyze the result')
 p.add_argument('-t', '--timestamp', type=str, default="", dest="timestamp", action="store", help="the timestamp of data file")
 p.add_argument('-n', '--nearnumber', type=int, default=0, dest="nearNumber", action="store", help="the number of nearest timestamp")
+p.add_argument('-d', '--detailoutput', default=False, dest="detailOutput", action="store_true", help="give more detail outputs")
 args = p.parse_args()
 
 data_root = '/proj/socnet-PG0/data/'
 if args.timestamp:
-    media_size_path = data_root + args.timestamp + '/mediaSize/'
+    experiment_path = data_root + args.timestamp
 else:
     files = os.listdir(data_root)
     files = sorted(files, reverse=True)
-    media_size_path = data_root + files[args.nearNumber] + '/mediaSize/'
+    experiment_path = data_root + files[args.nearNumber]
+
+media_size_path = experiment_path + '/mediaSize/'
+config_path = experiment_path + '/config.json'
+caching_policy = json.load(open(config_path, 'r'))['caching_policy']
 
 total_media_size = 0
+media_size_each_level = [0 for _ in range(3)]
 for ip_address in os.listdir(media_size_path):
     curr_path = media_size_path + ip_address + '/'
+    curr_level = math.floor(int(ip_address.split('.')[-1]) / 2)
     for file_name in os.listdir(curr_path):
         for line in open(curr_path + file_name, 'r'):
-            if line.strip():
-                total_media_size += int(line.strip())
+            curr_media_size = line.strip()
+            if curr_media_size:
+                total_media_size += int(curr_media_size)
+                media_size_each_level[curr_level] += int(curr_media_size)
 print(total_media_size)
+
+if args.detailOutput:
+    print("caching_policy: ", caching_policy)
+    for i in range(3):
+        print("level %s flow %s"%(str(i), media_size_each_level[i]))
