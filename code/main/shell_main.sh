4ce67ed113cac52e98a8506f81c6a59303c24dd4
skyerguo97 <skyerguo97@gmail.com>
2022-08-22 21:46:41 +0800

    add dict storage to accelerate

--- code/main/shell_main.sh
+++ code/main/shell_main.sh
@@ -1,15 +1,15 @@
-cat code/main/basic_config.json | jq '.caching_policy="LRU"' > code/main/config.json
-sudo python3 -m code.main.main
+# cat code/main/basic_config.json | jq '.caching_policy="LRU"' > code/main/config.json
+# sudo python3 -m code.main.main
 
-sleep 20
-cat code/main/basic_config.json | jq '.caching_policy="FIFO"' > code/main/config.json
-sudo python3 -m code.main.main
+# sleep 20
+# cat code/main/basic_config.json | jq '.caching_policy="FIFO"' > code/main/config.json
+# sudo python3 -m code.main.main
 
 # sleep 20
 # cat code/main/basic_config.json | jq '.caching_policy="RAND"' > code/main/config.json
 # sudo python3 -m code.main.main
 
-sleep 20
+# sleep 20
 cat code/main/basic_config.json | jq '.caching_policy="PageRank"' > code/main/config.json
 sudo python3 -m code.main.main
 
