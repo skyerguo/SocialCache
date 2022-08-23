074f1642b71b457a686450953c96869ee55e2604
skyerguo97 <skyerguo97@gmail.com>
2022-08-23 20:37:25 +0800

    完成一组初步的SocialMetrics实验

--- code/main/shell_social_metric.sh
+++ code/main/shell_social_metric.sh
@@ -1,19 +1,19 @@
-sleep 20
+sleep 5
 cat code/main/basic_config.json | jq '.caching_policy="Degree"' > code/main/config.json
 sudo python3 -m code.main.main
 
-sleep 20
+sleep 5
 cat code/main/basic_config.json | jq '.caching_policy="PageRank"' > code/main/config.json
 sudo python3 -m code.main.main
 
-sleep 20
-cat code/main/basic_config.json | jq '.caching_policy="Laplacian Centrality"' > code/main/config.json
+sleep 5
+cat code/main/basic_config.json | jq '.caching_policy="LaplacianCentrality"' > code/main/config.json
 sudo python3 -m code.main.main
 
-sleep 20
-cat code/main/basic_config.json | jq '.caching_policy="Betweenness Centrality"' > code/main/config.json
+sleep 5
+cat code/main/basic_config.json | jq '.caching_policy="BetweennessCentrality"' > code/main/config.json
 sudo python3 -m code.main.main
 
-sleep 20
-cat code/main/basic_config.json | jq '.caching_policy="Effective Size"' > code/main/config.json
+sleep 5
+cat code/main/basic_config.json | jq '.caching_policy="EffectiveSize"' > code/main/config.json
 sudo python3 -m code.main.main
