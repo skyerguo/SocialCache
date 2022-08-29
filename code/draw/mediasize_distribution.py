d789cad83c75076464e0e3a4005d392c53fd7331
skyerguo97 <skyerguo97@gmail.com>
2022-08-29 22:42:41 +0800

    upate mediasize distribution

--- code/draw/mediasize_distribution.py
+++ code/draw/mediasize_distribution.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import json
 import pandas as pd
 import numpy as np
-result_path = 'figures/meidasize_distribution.pdf'
+result_path = 'figures/mediasize_distribution.pdf'
 
 data_path = 'code/trace/media_size_sample/population'
 test_data = np.loadtxt(data_path, dtype='int') 
@@ -12,10 +12,8 @@ df = pd.DataFrame(test_data, columns=['Media Size (Bytes)'])
 
 if __name__ == '__main__':
     plt.rcParams["font.family"] = "Times New Roman"
-    # print(df)
-    # g = sns.lineplot(x='Level 1 Cache Size', y='Media File Size', hue='Method', style='Method', data=df)
-    g = sns.histplot(data=df, x="Media Size (Bytes)", log_scale=True)
-    g.set_xlim(1)
+    g = sns.histplot(data=df, x="Media Size (Bytes)", element="step", log_scale=True)
+    # g.set_xlim(1)
     g.spines['top'].set_visible(False)
     g.spines['right'].set_visible(False)
     plt.savefig(result_path, dpi=600, bbox_inches='tight')
