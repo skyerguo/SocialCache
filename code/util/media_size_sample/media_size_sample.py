0c38f53a0684efe5630fa51be529f9a85ad57b12
skyerguo97 <skyerguo97@gmail.com>
2022-09-07 10:49:51 +0800

    fix bugs of too much mediasize

--- code/util/media_size_sample/media_size_sample.py
+++ code/util/media_size_sample/media_size_sample.py
@@ -8,15 +8,19 @@ class media_size_sample:
         self.population = self.path + '/' + "population"
 
     def sample(self, num):
-        shell_cmd = "shuf %s -n %d" %(self.population, num)
-        ret = subprocess.Popen(shlex.split(shell_cmd), stdout=subprocess.PIPE)
-        result = ret.communicate()[0].decode('utf-8')
-        ret.wait()
+        max_len = 2352135
+        res_list = []
+        while num > 0:
+            shell_cmd = "shuf %s -n %d" %(self.population, min(num, max_len))
+            ret = subprocess.Popen(shlex.split(shell_cmd), stdout=subprocess.PIPE)
+            result = ret.communicate()[0].decode('utf-8')
+            ret.wait()
 
-        res_list = result.split()
+            res_list += result.split()
+            num -= max_len
         return list(map(int, res_list))
 
-if __name__ == "__main__":
-    sp = media_size_sample()
-    res = sp.sample(10)
-    print(res)
+# if __name__ == "__main__":
+#     sp = media_size_sample()
+#     res = sp.sample(10)
+#     print(res)
