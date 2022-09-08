202d17a6dde778d2e2b8f236e085113babe72f81
skyerguo97 <skyerguo97@gmail.com>
2022-09-08 21:59:42 +0800

    add a new try_LRU_social search method

--- code/main/shell_hill_climbing.sh
+++ code/main/shell_hill_climbing.sh
@@ -17,3 +17,9 @@ sudo python -m code.optimize.hill_climbing
 sleep 5
 cat code/main/basic_config.json | jq '.caching_policy="EffectiveSize"' > code/main/config.json
 sudo python -m code.optimize.hill_climbing
+
+sleep 5
+cat code/main/basic_config.json | jq '.caching_policy="LRU-social"' > code/main/config_tmp.json
+cat code/main/config_tmp.json | jq '.use_priority_queue=false' > code/main/config_tmp2.json
+cat code/main/config_tmp2.json | jq '.trace_dir="gtc_long_trace"' > code/main/config.json
+sudo python3 -m code.optimize.try_LRU_social  
