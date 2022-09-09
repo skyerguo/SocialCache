8118928bbbbac4742f683379ff4004ebc368f4b3
skyerguo97 <skyerguo97@gmail.com>
2022-09-09 20:52:12 +0800

    hill_climbing draw done

--- code/draw/parameter_fine_tune.py
+++ code/draw/parameter_fine_tune.py
@@ -6,15 +6,53 @@ import pandas as pd
 import numpy as np
 result_path = 'figures/parameter_fine_tune.pdf'
 
-data_path = 'code/optimize/optimize_trace.csv'
-df = pd.read_csv(data_path)
-df = df.fillna(method = 'ffill') # 使用前一列填充NAN
-df = df.drop(['Unnamed: 0'], axis=1)  #删除a列
+optimize_log_name = {
+    "Degree": "optimize.log2022-09-08 22:04:57",
+    "Pagerank": "optimize.log2022-09-09 10:26:24",
+    "LaplacianCentrality": "optimize.log2022-09-09 11:29:30",
+    "BetweennessCentrality": "optimize.log2022-09-09 12:48:51",
+    "EffectiveSize": "optimize.log2022-09-09 13:44:49"
+}
+
+def fetch_data(max_len=0):
+    df = pd.DataFrame()
+    df_dict = {}
+    for social_metric in optimize_log_name.keys():
+        temp_iteration = []
+        with open(optimize_log_name[social_metric], 'r') as f_in:
+            for line in f_in:
+                if "round" in line:
+                    now = -1
+                if "step" in line:
+                    now += 1
+                if "traffic" in line and "optimize" not in line:
+                    curr_media_size = float(line.split(":")[1].strip())
+                    if now >= len(temp_iteration):
+                        temp_iteration.append(curr_media_size)
+                        if now > 0:
+                            temp_iteration[now] = min(temp_iteration[now], temp_iteration[now-1])
+                    else:
+                        temp_iteration[now] = min(temp_iteration[now], curr_media_size)
+        if max_len > 0 and len(temp_iteration) > max_len:
+            temp_iteration = temp_iteration[:max_len]
+        df_dict[social_metric] = temp_iteration
+        
+    df = pd.DataFrame.from_dict(dict([(k,pd.Series(v)) for k,v in df_dict.items()]))
+    df = df.fillna(method = 'ffill') # 使用前一列填充NAN
+    # print(df)
+    return df
+    
+
+# data_path = 'code/optimize/optimize_trace.csv'
+# df = pd.read_csv(data_path)
+# df = df.fillna(method = 'ffill') # 使用前一列填充NAN
+# df = df.drop(['Unnamed: 0'], axis=1)  #删除a列
 
 if __name__ == '__main__':
+    df = fetch_data(161)
     plt.rcParams["font.family"] = "Times New Roman"
     g = sns.lineplot(data=df)
-    # g.set_ylim(0) # 加了图有点丑
     g.spines['top'].set_visible(False)
     g.spines['right'].set_visible(False)
+    g.legend(loc='upper right', frameon=False, title=None)
     plt.savefig(result_path, dpi=600, bbox_inches='tight')
--- code/main/shell_social_metric.sh
+++ code/main/shell_social_metric.sh
@@ -1,24 +1,24 @@
 sleep 5
 cat code/main/basic_config.json | jq '.caching_policy="Degree"' > code/main/config_tmp.json
-cat code/main/config_tmp.json | jq '.params=[75, 100, 0]' > code/main/config.json
+cat code/main/config_tmp.json | jq '.params=[0, 56.0, 13]' > code/main/config.json
 sudo python3 -m code.main.main
 
 sleep 5
 cat code/main/basic_config.json | jq '.caching_policy="PageRank"' > code/main/config_tmp.json
-cat code/main/config_tmp.json | jq '.params=[1, 680, 0]' > code/main/config.json
+cat code/main/config_tmp.json | jq '.params=[0, 64.5, 310000]' > code/main/config.json
 sudo python3 -m code.main.main
 
 sleep 5
 cat code/main/basic_config.json | jq '.caching_policy="LaplacianCentrality"' > code/main/config_tmp.json
-cat code/main/config_tmp.json | jq '.params=[1, 230, 0]' > code/main/config.json
+cat code/main/config_tmp.json | jq '.params=[0, 57.5, 54000]' > code/main/config.json
 sudo python3 -m code.main.main
 
 sleep 5
 cat code/main/basic_config.json | jq '.caching_policy="BetweennessCentrality"' > code/main/config_tmp.json
-cat code/main/config_tmp.json | jq '.params=[1, 270, 0]' > code/main/config.json
+cat code/main/config_tmp.json | jq '.params=[0, 54.0, 52000]' > code/main/config.json
 sudo python3 -m code.main.main
 
 sleep 5
 cat code/main/basic_config.json | jq '.caching_policy="EffectiveSize"' > code/main/config_tmp.json
-cat code/main/config_tmp.json | jq '.params=[108, 100, 1]' > code/main/config.json
+cat code/main/config_tmp.json | jq '.params=[0, 60.0, 103]' > code/main/config.json
 sudo python3 -m code.main.main
