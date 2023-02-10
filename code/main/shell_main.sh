7de69dcc7bd9e945b43dd7500f229eb1fca2dd63
skyerguo97 <skyerguo97@gmail.com>
2023-02-10 16:28:03 +0800

    update shell_main

--- code/main/shell_main.sh
+++ code/main/shell_main.sh
@@ -41,8 +41,12 @@
 # for i in `seq 0 20`; do
 #     echo $i
 #     # break
-#     python3 -m code.analyze.main -l -m -e -c -p -d -z -n $i
+#     python3 -m code.analyze.main -l -m -e -c -p -d -z -n -y $i
 # done
+
+## TwitterSmall
+cat code/main/basic_config.json | jq '.trace_dir="TwitterSmall"' > code/main/config_tmp.json
+cat code/main/config_tmp.json | jq '.params=[0, 60.0, 103]' > code/main/config.json
 
 sleep 5
 cat code/main/basic_config.json | jq '.caching_policy="RAND"' > code/main/config.json
@@ -56,22 +60,22 @@ sleep 5
 cat code/main/basic_config.json | jq '.caching_policy="LRU"' > code/main/config.json
 sudo python3 -m code.main.main
 
-# sleep 5
-# cat code/main/basic_config.json | jq '.caching_policy="LRU-label"' > code/main/config_tmp.json
-# cat code/main/config_tmp.json | jq '.use_priority_queue=false' > code/main/config.json
-# sudo python3 -m code.main.main
-
-sleep 5
-cat code/main/basic_config.json | jq '.caching_policy="EffectiveSize"' > code/main/config_tmp.json
-cat code/main/config_tmp.json | jq '.params=[0, 60.0, 103]' > code/main/config.json
-sudo python3 -m code.main.main
-
 sleep 5
 cat code/main/basic_config.json | jq '.caching_policy="LRU-Social"' > code/main/config_tmp.json
 cat code/main/config_tmp.json | jq '.use_priority_queue=false' > code/main/config.json
 sudo python3 -m code.main.main
 
-# sleep 5
-# cat code/main/basic_config.json | jq '.caching_policy="PageRank"' > code/main/config_tmp.json
-# cat code/main/config_tmp.json | jq '.params=[0, 64.5, 310000]' > code/main/config.json
-# sudo python3 -m code.main.main
+sleep 5
+cat code/main/basic_config.json | jq '.caching_policy="EffectiveSize"' > code/main/config_tmp.json
+cat code/main/config_tmp.json | jq '.use_priority_queue=true' > code/main/config.json
+sudo python3 -m code.main.main
+
+sleep 5
+cat code/main/basic_config.json | jq '.caching_policy="EffectiveSize"' > code/main/config_tmp.json
+cat code/main/config_tmp.json | jq '.use_priority_queue=false' > code/main/config.json
+sudo python3 -m code.main.main
+
+for i in `seq 0 20`; do
+    echo $i
+    python3 -m code.analyze.main -l -m -e -c -p -d -q -y -z -n $i
+done
