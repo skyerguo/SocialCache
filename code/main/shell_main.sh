92a88658846836e8f0da948dfe3eb2215988ad09
skyerguo97 <skyerguo97@gmail.com>
2023-02-11 13:25:03 +0800

    BrightkiteCommunity result done

--- code/main/shell_main.sh
+++ code/main/shell_main.sh
@@ -36,13 +36,46 @@
 # sudo python3 -m code.main.main
 
 # for i in `seq 0 5`; do
-#     # echo $i
 #     python3 -m code.analyze.main -l -m -e -c -p -d -q -y -z -n $i
 # done
 
-## TwitterLarge
-cat code/main/basic_config.json | jq '.trace_dir="TwitterLarge"' > code/main/config_tmp.json
-cat code/main/config_tmp.json | jq '.params=[0.05, 50.5, 90]' > code/main/basic_config.json
+# ## TwitterLarge
+# cat code/main/basic_config.json | jq '.trace_dir="TwitterLarge"' > code/main/config_tmp.json
+# cat code/main/config_tmp.json | jq '.params=[0.05, 50.5, 90]' > code/main/basic_config.json
+
+# sleep 5
+# cat code/main/basic_config.json | jq '.caching_policy="RAND"' > code/main/config.json
+# sudo python3 -m code.main.main
+
+# sleep 5
+# cat code/main/basic_config.json | jq '.caching_policy="FIFO"' > code/main/config.json
+# sudo python3 -m code.main.main
+
+# sleep 5
+# cat code/main/basic_config.json | jq '.caching_policy="LRU"' > code/main/config.json
+# sudo python3 -m code.main.main
+
+# sleep 5
+# cat code/main/basic_config.json | jq '.caching_policy="LRU-Social"' > code/main/config_tmp.json
+# cat code/main/config_tmp.json | jq '.use_priority_queue=false' > code/main/config.json
+# sudo python3 -m code.main.main
+
+# sleep 5
+# cat code/main/basic_config.json | jq '.caching_policy="EffectiveSize"' > code/main/config.json
+# sudo python3 -m code.main.main
+
+# sleep 5
+# cat code/main/basic_config.json | jq '.caching_policy="EffectiveSize"' > code/main/config_tmp.json
+# cat code/main/config_tmp.json | jq '.use_priority_queue=false' > code/main/config.json
+# sudo python3 -m code.main.main
+
+# for i in `seq 0 5`; do
+#     python3 -m code.analyze.main -l -m -e -c -p -d -q -y -z -n $i
+# done
+
+## BrightkiteBigraphCommunity
+cat code/main/basic_config.json | jq '.trace_dir="BrightkiteBigraphCommunity"' > code/main/config_tmp.json
+cat code/main/config_tmp.json | jq '.params=[200, 0.0035, 10]' > code/main/basic_config.json
 
 sleep 5
 cat code/main/basic_config.json | jq '.caching_policy="RAND"' > code/main/config.json
@@ -71,6 +104,5 @@ cat code/main/config_tmp.json | jq '.use_priority_queue=false' > code/main/confi
 sudo python3 -m code.main.main
 
 for i in `seq 0 5`; do
-    # echo $i
     python3 -m code.analyze.main -l -m -e -c -p -d -q -y -z -n $i
 done
