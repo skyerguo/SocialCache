f8416874eb19e8bcdd0acd42b9ddc30b25e708ac
skyerguo97 <skyerguo97@gmail.com>
2023-02-13 13:03:25 +0800

    GowallaBigraphCommunity初步结果完成

--- code/main/shell_main.sh
+++ code/main/shell_main.sh
@@ -73,8 +73,42 @@
 #     python3 -m code.analyze.main -l -m -e -c -p -d -q -y -z -n $i
 # done
 
-## BrightkiteBigraphCommunity
-cat code/main/basic_config.json | jq '.trace_dir="BrightkiteBigraphCommunity"' > code/main/config_tmp.json
+# ## BrightkiteBigraphCommunity
+# cat code/main/basic_config.json | jq '.trace_dir="BrightkiteBigraphCommunity"' > code/main/config_tmp.json
+# cat code/main/config_tmp.json | jq '.params=[200, 0.0035, 10]' > code/main/basic_config.json
+
+# sleep 5
+# cat code/main/basic_config.json | jq '.caching_policy="RAND"' > code/main/config.json
+# sudo python3 -m code.main.main
+
+# sleep 5
+# cat code/main/basic_config.json | jq '.caching_policy="FIFO"' > code/main/config.json
+# sudo python3 -m code.main.main
+
+# sleep 5
+# cat code/main/basic_config.json | jq '.caching_policy="LRU"' > code/main/config.json
+# sudo python3 -m code.main.main
+
+# sleep 5
+# cat code/main/basic_config.json | jq '.caching_policy="LRU-Social"' > code/main/config_tmp.json
+# cat code/main/config_tmp.json | jq '.use_priority_queue=false' > code/main/config.json
+# sudo python3 -m code.main.main
+
+# sleep 5
+# cat code/main/basic_config.json | jq '.caching_policy="EffectiveSize"' > code/main/config.json
+# sudo python3 -m code.main.main
+
+# sleep 5
+# cat code/main/basic_config.json | jq '.caching_policy="EffectiveSize"' > code/main/config_tmp.json
+# cat code/main/config_tmp.json | jq '.use_priority_queue=false' > code/main/config.json
+# sudo python3 -m code.main.main
+
+# for i in `seq 0 5`; do
+#     python3 -m code.analyze.main -l -m -e -c -p -d -q -y -z -n $i
+# done
+
+## GowallaBigraphCommunity
+cat code/main/basic_config.json | jq '.trace_dir="GowallaBigraphCommunity"' > code/main/config_tmp.json
 cat code/main/config_tmp.json | jq '.params=[200, 0.0035, 10]' > code/main/basic_config.json
 
 sleep 5
